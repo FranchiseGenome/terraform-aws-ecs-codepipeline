009fe2821c10a6490464271c993daaa5b1ee1923
davearata <dave.arata@dacatech.com>
2019-09-09 08:11:48 -0400

    set codebuild module to master to fix error

--- main.tf
+++ main.tf
@@ -172,7 +172,7 @@ data "aws_caller_identity" "default" {}
 data "aws_region" "default" {}
 
 module "build" {
-  source                = "git::https://github.com/cloudposse/terraform-aws-codebuild.git?ref=tags/0.16.0"
+  source                = "git::https://github.com/cloudposse/terraform-aws-codebuild.git?ref=master"
   enabled               = "${var.enabled}"
   namespace             = "${var.namespace}"
   name                  = "${var.name}"
